d1a0ac70cb7cfa0080ee21dd7793a86ba0c98a1d
PJ Gray <pj4533@gmail.com>
2025-04-19 10:03:20 -0400

    Initial Commit

---
+++ LostArchiveTV/LostArchiveTVApp.swift
@@ -0,0 +1,17 @@
+//
+//  LostArchiveTVApp.swift
+//  LostArchiveTV
+//
+//  Created by PJ Gray on 4/19/25.
+//
+
+import SwiftUI
+
+@main
+struct LostArchiveTVApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
